c3df0247d4b988c6d760161d719027448bf50703
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-16 10:14:22 -0300

    Adds exercise 8/easy.

---
+++ code/8/easy.py
@@ -0,0 +1,8 @@
+# Crie um programa que verifique se um número é positivo, negativo ou zero.
+number = float(input("Digite um número: "))
+if number > 0:
+  print("O número é positivo.")
+elif number < 0:
+  print("O número é negativo.")
+else:
+  print("O número é zero.")
